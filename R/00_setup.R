f99da8259737f02e4a8e5b86570205d6fa377e9f
abemorc <abemorc00@gmail.com>
2021-12-01 20:55:59 -0600

    Update

---
+++ R/00_setup.R
@@ -0,0 +1,42 @@
+
+# librerias usadas by book mastering shiny hadley wickham -----------------
+
+
+# revisar si el paquete pacman esta instalado
+# if (!require("pacman")) install.packages("pacman")
+# # para citar paquetes y gestion de los mismos
+# library(pacman)
+
+
+
+# Instalacion librerias faltantes -----------------------------------------
+
+oldw <- getOption("warn")
+options(warn = -1)
+
+packages <- c("gapminder", "ggforce", "gh", "globals", "openintro", "profvis", 
+              "RSQLite", "shiny", "shinycssloaders", "shinyFeedback", 
+              "shinythemes", "testthat", "thematic", "tidyverse", "vroom", 
+              "waiter", "xml2", "zeallot", "fun")
+
+installed_packages <- packages %in% rownames(installed.packages())
+if (any(installed_packages == FALSE)) {
+  install.packages(packages[!installed_packages])
+}
+
+options(warn = oldw)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
